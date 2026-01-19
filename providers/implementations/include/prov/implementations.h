fea59c3f7726025e2f5d03fed35b545e767bdcae
Paul Louvel <paul.louvel@bootlin.com>
2026-01-19 19:16:57 +0100

    fix: added missing conditional macro when disabling the SipHash algorithm
    Original-Merge-Date: Fri Mar 13 15:50:39 2026
    Original-PR: https://github.com/openssl/openssl/pull/30212
    Original-Commit: 6c1fead89023 "fix: added missing conditional macro when disabling the SipHash algorithm"
    Resolves: https://github.com/openssl/openssl/issues/29514
    Fixes: b27b31b62846 "Extend the provider MAC bridge for SIPHASH"
    
    Reviewed-by: Nikola Pajkovsky <nikolap@openssl.org>
    Reviewed-by: Saša Nedvědický <sashan@openssl.org>
    MergeDate: Wed Apr 22 07:44:46 2026
    (Merged from https://github.com/openssl/openssl/pull/30897)

--- providers/implementations/include/prov/implementations.h
+++ providers/implementations/include/prov/implementations.h
@@ -248,7 +248,9 @@ extern const OSSL_DISPATCH ossl_gmac_functions[];
 extern const OSSL_DISPATCH ossl_hmac_functions[];
 extern const OSSL_DISPATCH ossl_kmac128_functions[];
 extern const OSSL_DISPATCH ossl_kmac256_functions[];
+#ifndef OPENSSL_NO_SIPHASH
 extern const OSSL_DISPATCH ossl_siphash_functions[];
+#endif
 extern const OSSL_DISPATCH ossl_poly1305_functions[];
 
 /* KDFs / PRFs */
