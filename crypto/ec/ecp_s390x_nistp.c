43233bd20938143e2e228295070cb8f7a5e545cb
Joshua Rogers <MegaManSec@users.noreply.github.com>
2026-03-31 23:53:47 +0800

    ecp_s390x_nistp.c: Reject negative digest length to prevent size_t underflow
    Reviewed-by: Eugene Syromiatnikov <esyr@openssl.org>
    Reviewed-by: Frederik Wedel-Heinen <fwh.openssl@gmail.com>
    Reviewed-by: Paul Dale <paul.dale@oracle.com>
    Reviewed-by: Tomas Mraz <tomas@openssl.foundation>
    MergeDate: Wed Apr 15 11:10:38 2026
    (Merged from https://github.com/openssl/openssl/pull/30648)

--- crypto/ec/ecp_s390x_nistp.c
+++ crypto/ec/ecp_s390x_nistp.c
@@ -142,6 +142,11 @@ static ECDSA_SIG *ecdsa_s390x_nistp_sign_sig(const unsigned char *dgst,
     const BIGNUM *privkey;
     int off;
 
+    if (dgstlen < 0) {
+        ERR_raise(ERR_LIB_EC, EC_R_INVALID_LENGTH);
+        return NULL;
+    }
+
     group = EC_KEY_get0_group(eckey);
     privkey = EC_KEY_get0_private_key(eckey);
     if (group == NULL || privkey == NULL) {
@@ -238,6 +243,11 @@ static int ecdsa_s390x_nistp_verify_sig(const unsigned char *dgst, int dgstlen,
     const EC_GROUP *group;
     const EC_POINT *pubkey;
     int off;
+
+    if (dgstlen < 0) {
+        ERR_raise(ERR_LIB_EC, EC_R_INVALID_LENGTH);
+        return -1;
+    }
 
     group = EC_KEY_get0_group(eckey);
     pubkey = EC_KEY_get0_public_key(eckey);
